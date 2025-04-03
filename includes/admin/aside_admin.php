703efb624e93e7ef444e8bd381dd6f11b02600b6
ZakariaBj <zakariabenjeddi22@gmail.com>
2025-04-03 15:46:41 +0100

    fix navbar la page d'absence

--- includes/admin/aside_admin.php
+++ includes/admin/aside_admin.php
@@ -116,7 +116,7 @@
 				<h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Suivi</h6>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="../admin/absences.php">
+				<a class="nav-link" href="../admin/absence.php">
 					<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
 						<i class="ni ni-user-run text-danger text-sm opacity-10"></i>
 					</div>
