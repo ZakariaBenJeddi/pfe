8cbe743f3ea1f9e4dac0dc68b99f069471f97bb8
ZakariaBj <zakariabenjeddi22@gmail.com>
2025-05-24 19:23:38 +0100

    affichage des routes

---
+++ pages/admin/routes.php
@@ -0,0 +1,175 @@
+<?php
+include('../../includes/admin/controller/controller.php');
+
+//** Configuration sécurisée des sessions
+ini_set('session.cookie_secure', 1); // Cookies uniquement via HTTPS
+ini_set('session.cookie_httponly', 1); // Cookies inaccessibles via JavaScript
+ini_set('session.use_strict_mode', 1); // Empêche l'utilisation de sessions non valides
+
+session_start();
+
+//* Vérification de l'authentification
+if (empty($_SESSION['user'])) {
+  header('location:../sign-in.php');
+  exit();
+}
+
+//* deconnexion
+require('../../includes/deconnexion_5s.php');
+
+//* Read
+try {
+  $results = get_all_routes($dbh);
+} catch (PDOException $e) {
+  error_log($e->getMessage(), 3, '/path/to/secure_log_file.log');
+  die("Erreur lors de la récupération des données.");
+}
+
+//* Delete
+if (!empty($_GET['id']) && isset($_GET['del']) && $_GET['del'] === '1') {
+  $result = delete_route($dbh, $_GET['id']);
+  if ($result['success']) {
+      header("Location: routes.php?success=1");
+      exit();
+  } else {
+      $errorMessage = urlencode($result['message']);
+      header("Location: routes.php?error={$errorMessage}");
+      exit();
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- HEAD -->
+<?php include '../../includes/admin/head_admin.php' ?>
+
+<body class="g-sidenav-show   bg-gray-100">
+  <div class="min-height-300 bg-primary position-absolute w-100"></div>
+  <?php require('../../includes/admin/aside_admin.php') ?>
+  <main class="main-content position-relative border-radius-lg ">
+    <!-- Navbar -->
+    <?php require('../../includes/admin/navbar_admin.php') ?>
+    <!-- End Navbar -->
+    <div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-12">
+          <div class="card mb-4">
+            <div class="card-header pb-0 d-flex flex-wrap justify-content-between align-items-center text-center text-md-start">
+              <div class="mb-2 mb-md-0 flex-grow-1 text-center text-md-start">
+                <h6 class="text-primary">Filière</h6>
+              </div>
+              <div class="d-flex flex-column flex-md-row justify-content-center justify-content-md-end align-items-center gap-2 w-100">
+                <a class="btn btn-primary btn-sm" href="ajouter_route.php">Ajouter Route</a>
+                <button type="button" class="btn btn-primary btn-sm" onclick="expo()" id="btnexp">Exporter</button>
+              </div>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nom Route</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">start latitude</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">start longitude</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">end latitude</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">end longitude</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody id="tableBody">
+                    <?php if (count($results) > 0) { ?>
+                      <?php foreach ($results as $result) : ?>
+                        <tr>
+                          <td>
+                            <div class="d-flex px-2 py-1">
+                              <div>
+                                <img src="../../assets/img/small-logos/logo-invision.svg" class="avatar avatar-sm me-3" alt="filiere">
+                              </div>
+                              <div class="d-flex flex-column justify-content-center">
+                                <h6 class="mb-0 text-sm"><?= $result->route_name ?></h6>
+                              </div>
+                            </div>
+                          </td>
+                          <td class="align-middle text-center">
+                            <p class="text-xs font-weight-bold mb-0">
+                              <?= $result->start_latitude ?>
+                            </p>
+                          </td>
+                          <td class="align-middle text-center">
+                            <p class="text-xs font-weight-bold mb-0">
+                              <?= $result->start_longitude ?>
+                            </p>
+                          </td>
+                          <td class="align-middle text-center">
+                            <p class="text-xs font-weight-bold mb-0">
+                              <?= $result->end_latitude ?>
+                            </p>
+                          </td>
+                          <td class="align-middle text-center">
+                            <p class="text-xs font-weight-bold mb-0">
+                              <?= $result->end_longitude ?>
+                            </p>
+                          </td>
+                          <td class="align-middle text-center">
+                            <div class="d-flex">
+                              <a href="edit_route.php?id=<?= $result->route_id ?>" class="dropdown-item">
+                                <i class="fas fa-pencil-alt text-dark opacity-8 fa-sm" aria-hidden="true"></i>
+                              </a>
+                              <a href="routes.php?id=<?= $result->route_id ?>&del=1" class="dropdown-item" onClick="return confirmDelete(event, this)">
+                                <i class="fas fa-trash fa-sm text-danger opacity-8"></i>
+                              </a>
+                            </div>
+                          </td>
+                        </tr>
+                      <?php endforeach; ?>
+                    <?php } else { ?>
+                      <tr rowspan="7" class="text-center">
+                        <td class="text-center">
+                          No Content
+                        </td>
+                      </tr>
+                    <?php  } ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FOOTER -->
+      <?php include '../../includes/footer.php' ?>
+
+    </div>
+  </main>
+
+  <!-- Data table -->
+  <script src="../../assets/js/datatable.js"></script>
+  <!-- Export Functio -->
+  <script src="../../assets/js/export.js"></script>
+
+  <!-- sweet alert -->
+  <script src="../../assets/js/alerts/sweet_alert.js"></script>
+
+  <!-- FIXED PLUGIN  -->
+  <?php include '../../includes/fixedplugin.php' ?>
+  <!--   Core JS Files   -->
+  <script src="../../assets/js/core/popper.min.js"></script>
+  <script src="../../assets/js/core/bootstrap.min.js"></script>
+  <script src="../../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
+</body>
+
+</html>
