7907f045bad273fe191cbf692f2f45d28a185217
ZakariaBj <zakariabenjeddi22@gmail.com>
2025-05-29 17:55:36 +0100

    affichage de section des routes dans le sidebar

--- includes/admin/aside_admin.php
+++ includes/admin/aside_admin.php
@@ -151,6 +151,18 @@
 					<span class="nav-link-text ms-1">Salles</span>
 				</a>
 			</li>
+			<li class="nav-item">
+				<h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Gestion des Transports</h6>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="../admin/routes.php">
+					<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="ni ni-map-big text-warning text-sm opacity-10"></i>
+					</div>
+					<span class="nav-link-text ms-1">Routes</span>
+				</a>
+			</li>
+
 			<!-- <li class="nav-item">
 				<a class="nav-link" href="../admin/equipements.php">
 					<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
